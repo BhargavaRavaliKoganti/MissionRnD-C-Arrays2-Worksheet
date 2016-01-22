754540c010b38255ff5a54af8f2a23896ba89bbd
BhargavaRavaliKoganti <BhargavaRavaliKoganti@missionrnd.com>
2016-01-22 19:42:45 +0530

    Submit Commit

--- src/mergeSortedArrays.cpp
+++ src/mergeSortedArrays.cpp
@@ -14,6 +14,7 @@ NOTES:
 */
 
 #include <iostream>
+#include<stdio.h>
 
 struct transaction {
 	int amount;
@@ -21,6 +22,86 @@ struct transaction {
 	char description[20];
 };
 
+int* DateConverter(char *);
+
 struct transaction * mergeSortedArrays(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	return NULL;
+	if (A == NULL || B == NULL)
+		return NULL;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	struct transaction *result = (struct transaction*)malloc(sizeof(struct transaction) * (ALen+BLen));
+	while (i < ALen && j < BLen){
+		int *ADate = DateConverter(A[i].date);
+		int *BDate = DateConverter(B[j].date);
+		if (ADate[2] < BDate[2]){
+			result[k] = A[i];
+			i++;
+			k++;
+		}
+		else if (ADate[2] > BDate[2]){
+			result[k] = B[j];
+			k++;
+			j++;
+		}
+		else{
+			if (ADate[1] < BDate[1]){
+				result[k] = A[i];
+				i++;
+				k++;
+			}
+			else if (ADate[1] > BDate[1]){
+				result[k] = B[j];
+				k++;
+				j++;
+			}
+			else{
+				if (ADate[0] < BDate[0]){
+					result[k] = A[i];
+					i++;
+					k++;
+				}
+				else if (ADate[0] > BDate[0]){
+					result[k] = B[j];
+					k++;
+					j++;
+				}
+				else{
+					result[k] = A[i];
+					i++;
+					k++;
+				}
+			}
+		}
+	}
+	if (i < ALen) {
+		for (int index = i; index < ALen; index++) {
+			result[k] = A[index];
+			k++;
+		}
+	}
+	else {
+		for (int index = j; index < BLen; index++) {
+			result[k] = B[index];
+			k++;
+		}
+	}
+	return result;
+}
+
+int* DateConverter(char *date){
+	int j = 0;
+	int num = 0;
+	int * x = (int *)malloc(sizeof(int) * 3);
+	for (int i = 0; date[i] != '\0'; i++){
+		if (date[i] == '-'){
+			x[j] = num / 10;
+			j += 1;
+			num = 0;
+		}
+		else
+			num = (num + (date[i] - '0')) * 10;
+	}
+	x[j] = num / 10;
+	return x;
 }
