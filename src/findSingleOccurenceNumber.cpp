bb22f8fe35b85ca9ff10d25299c9c5355af3223f
BhargavaRavaliKoganti <BhargavaRavaliKoganti@missionrnd.com>
2016-01-25 21:54:21 +0530

    Submit Commit

--- src/findSingleOccurenceNumber.cpp
+++ src/findSingleOccurenceNumber.cpp
@@ -12,7 +12,30 @@ ERROR CASES: Return -1 for invalid inputs.
 
 NOTES:
 */
+#include<stdio.h>
+
+void sortArray(int *, int);
 
 int findSingleOccurenceNumber(int *A, int len) {
+	if (A == NULL)
+		return -1;
+	sortArray(A, len);
+	for (int index = 0; index < len; index += 3)
+		if (index == len - 1 || A[index] != A[index + 1])
+			return A[index];
 	return -1;
+}
+
+void sortArray(int * given_array, int no_of_elements){
+	int temp;
+	for (int index = 0; index < no_of_elements - 1; index++){
+		for (int idx = 0; idx < no_of_elements - 1 - index; idx++){
+			if (given_array[idx] > given_array[idx + 1]){
+				temp = given_array[idx];
+				given_array[idx] = given_array[idx + 1];
+				given_array[idx + 1] = temp;
+			}
+		}
+	}
+	return;
 }
